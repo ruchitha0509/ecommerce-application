e349a9520dc05395b870630dd03697c0cc6038b5
ruchitha0509 <ruchithathallapelly81@gmail.com>
2025-08-06 22:48:30 +0530

    success

---
+++ ecom-backend/src/main/java/com/excelr/controller/ExcelRController.java
@@ -0,0 +1,49 @@
+package com.excelr.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.excelr.model.Headphones;
+import com.excelr.model.Laptops;
+import com.excelr.model.Login;
+import com.excelr.model.Mobiles;
+import com.excelr.service.ExcelRService;
+
+@RestController
+@CrossOrigin
+public class ExcelRController {
+     
+	@Autowired
+	private ExcelRService excelRService;
+	
+	@GetMapping("/laptops")
+	public List<Laptops> getAllLaptops(){
+		return excelRService.getAllLaptops();
+	}
+	
+	@GetMapping("/mobiles")
+	public List<Mobiles> getAllMobiles(){
+		return excelRService.getAllMobiles();
+	}
+	
+	@GetMapping("/headphones")
+	public List<Headphones> hetAllMobiles(){
+	        return excelRService.getAllHeadphones();
+	
+	}
+	@PostMapping("/login")
+	public String login(@RequestBody Login login) {
+	    Login loginResponse = excelRService.performLogin(login.getUsername());
+	    if(loginResponse.getPassword().equals(login.getPassword())) {
+	    	return "Login success";
+	    }else {
+	    	return "Login failed";
+	    }
+	}
+}
